3eb4189da75b8287e669b8c350166a7f15d66a35
maddynuni <110168661+maddynuni@users.noreply.github.com>
2023-12-23 18:48:42 +1100

    fixed page scaling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../styles/app.css';
 import Navbar from '../Navbar.js';
+import '../../styles/constants.css';
+import '../../styles/home.css';
 
 function Home () {
     return (
         <div className='Page' id='Home'>
             <header className="App-header">
-                <p>Madeleine Neaves</p>
+                <h4>Madeleine Neaves</h4>
             </header>
             <Navbar />
         </div>
